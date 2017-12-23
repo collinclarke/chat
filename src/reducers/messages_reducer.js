0d078125eb056e8b3fb5e6bf7d7fde75b5d34c44
Collin <collinclarke@gmail.com>
2017-12-23 13:20:36 -0800

    Clear messages on logout

--- src/reducers/messages_reducer.js
+++ src/reducers/messages_reducer.js
@@ -1,4 +1,5 @@
-import { RECEIVE_MESSAGE } from '../actions/message_actions';
+import { RECEIVE_MESSAGE, RECEIVE_MESSAGES } from '../actions/message_actions';
+import { LOGOUT_USER } from '../actions/session_actions';
 
 const initialState = [];
 
@@ -6,6 +7,8 @@ const messagesReducer = (state = initialState, action) => {
   switch(action.type){
     case RECEIVE_MESSAGE:
       return state.concat(action.message);
+    case LOGOUT_USER:
+      return initialState;
     default:
       return state;
   }
