0e232f6639cf8202c95635dcdbccc889abe9dd4c
Collin <collinclarke@gmail.com>
2017-12-23 09:58:19 -0800

    Complete session component

--- src/FacebookButton.js
+++ src/FacebookButton.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { loginUserWithFacebook } from './actions/session_actions';
+import { loginUserWithFacebook, logoutUser } from './actions/session_actions';
 
 class FacebookButton extends Component {
+
   render() {
+    const { currentUser } = this.props;
+    let msg, btnStyle;
+    if (currentUser) {
+      msg = "Logout";
+      btnStyle = "btn btn-default"
+    } else {
+      msg = "Continue with Facebook";
+      btnStyle = "btn btn-primary";
+    }
     return (
-      <button type="button" onClick={this.props.login} className="btn btn-primary">Continue with Facebook</button>
+      <button type="button" onClick={this.sessionAction} className={btnStyle}>{msg}</button>
     )
   }
+
+  sessionAction = () => {
+    const { currentUser, login, logout } = this.props;
+    currentUser ? logout() : login();
+  }
+
 }
 
 const mapDispatchToProps = dispatch => {
   return {
     login: () => dispatch(loginUserWithFacebook()),
+    logout: () => dispatch(logoutUser())
   };
 };
 
