a37685ad5bc40b2b3fc43a998552b81d7cdafedd
Collin <collinclarke@gmail.com>
2017-12-26 22:34:47 -0800

    Remove header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
 import { FeatureRoute } from './routeUtil/routes';
+import { Route } from 'react-router-dom';
 import FacebookButton from './FacebookButton';
 import Chatroom from './Chatroom';
 import Profile from './Profile';
+
 
 class App extends Component {
 
   render() {
     return (
       <div className="container-fluid">
-        <div className="row justify-content-center">
-          <div className="col-sm-12">
-            <h1 className="text-center">Welcome to Chat</h1>
-          </div>
-        </div>
-        <div className="row fixed-top">
-          <div className="col-sm-12 justify-content-center">
+        <div className="row fixed-top m-3">
+          <div className="col-4 align-self-start">
             <FacebookButton />
           </div>
         </div>
+        <Route exact path='/profile' component={ Profile }/>
         <FeatureRoute exact path='/chat' component={ Chatroom } />
-        <FeatureRoute exact path='/profile' component={ Profile }/>
       </div>
     );
   }
