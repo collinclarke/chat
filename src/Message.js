dd0cc502036862be27036e230fc41a2bf1a4791c
Collin <collinclarke@gmail.com>
2017-12-26 22:37:29 -0800

    Styled messages and added scroll into view

--- src/Message.js
+++ src/Message.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
 class Message extends Component {
+  constructor( props ) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.refs.message.scrollIntoView({block: 'start', behavior: 'smooth'})
+  }
+
   render() {
     return (
-      <div className="row justify-content-center">
-        <div className="col-sm-8">
-          <div className="message text-center">
+      <div className="row justify-content-end">
+        <div className="col-auto mw-100">
+          <div ref="message" className="text-right rounded bg-primary m-2 p-2 text-light">
             { this.props.text }
           </div>
         </div>
