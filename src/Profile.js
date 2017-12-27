1079bc96bbc4cb1cf7afcf80404d8f6cd1cdf3d7
Collin <collinclarke@gmail.com>
2017-12-26 22:38:02 -0800

    Add profile image

--- src/Profile.js
+++ src/Profile.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import { updateProfile } from './actions/session_actions';
 
 class Profile extends Component {
+
   render() {
+    const { photoURL } = this.props.currentUser;
     return (
-      <div className="row">
-        <div className="col-sm-10 justify-content-sm-center">
-          User
+      <div className="row justify-content-center">
+        <div className="col-3">
+          <img src={photoURL}
+          className="rounded-circle"
+          alt="Profile"/>
         </div>
       </div>
     );
   }
+
 }
 
-export default Profile;
+const mapDispatchToProps = dispatch => {
+  return {
+    updateProfile: (data) => dispatch(updateProfile(data))
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    currentUser: state.session.currentUser
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
