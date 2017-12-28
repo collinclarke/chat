b04502065dd46ca442f0d8335842ec1c65fc6164
Collin <collinclarke@gmail.com>
2017-12-28 10:16:27 -0800

    Build out profile and include editable bio

--- src/Profile.js
+++ src/Profile.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { updateProfile } from './actions/session_actions';
+import { saveBio } from './actions/session_actions';
 
 class Profile extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      bio: "",
+      editing: false
+    }
+  }
+
+  handleChange = e => {
+    this.setState({bio: e.currentTarget.value})
+  }
+
+  updateBio = e => {
+    this.props.saveBio(this.state.bio);
+    this.setState({editing: false})
+  }
+
   render() {
-    const { photoURL } = this.props.currentUser;
+    const { photoURL, displayName } = this.props.currentUser;
     return (
-      <div className="row justify-content-center">
-        <div className="col-3">
-          <img src={photoURL}
-          className="rounded-circle"
-          alt="Profile"/>
+      <div>
+        <div className="row justify-content-center">
+          <div className="col-6 text-center">
+            <img src={photoURL}
+            className="rounded-circle my-3"
+            alt="Profile"/>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-6 text-center">
+            <h4>{ displayName }</h4>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-12 text-center">
+            <textarea onChange={this.handleChange} placeholder="Say something about yourself..."></textarea>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-5 text-center">
+            <button type="button" onClick={this.updateBio} className="btn">Update Profile</button>
+          </div>
         </div>
       </div>
     );
@@ -21,7 +55,7 @@ class Profile extends Component {
 
 const mapDispatchToProps = dispatch => {
   return {
-    updateProfile: (data) => dispatch(updateProfile(data))
+    saveBio: (bio) => dispatch(saveBio(bio))
   }
 }
 
