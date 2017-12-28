d2ea1ea4d41650601cc6cb0a8f705b9c99ad2f2c
Collin <collinclarke@gmail.com>
2017-12-28 10:28:00 -0800

    Add login page

--- src/Login.js
+++ src/Login.js
@@ -4,9 +4,14 @@ import FacebookButton from './FacebookButton';
 class Login extends Component {
   render() {
     return (
-      <div className="row fixed-top m-3">
-        <div className="row">
-          <div className="col-4 align-self-start">
+      <div>
+        <div className="row justify-content-center">
+          <div className="col-10 text-center">
+            <h3>Welcome to Chat</h3>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-10 text-center">
             <FacebookButton />
           </div>
         </div>
