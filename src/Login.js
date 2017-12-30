4f7cae3d589aa97d17b95a1d42a0fc034bd777ff
Collin <collinclarke@gmail.com>
2017-12-30 12:31:26 -0800

    Add logo and center login screen

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import FacebookButton from './FacebookButton';
+import logoLarge from './assets/talk_large.svg';
 
 class Login extends Component {
   render() {
     return (
-      <div>
+      <div style={{height: '100vh'}} className="d-flex flex-column justify-content-center">
         <div className="row justify-content-center">
           <div className="col-10 text-center">
-            <h3>Welcome to Chat</h3>
+            <img style={{width: "70vw", maxWidth: "20rem"}} src={logoLarge} />
           </div>
         </div>
         <div className="row justify-content-center">
-          <div className="col-10 text-center">
+          <div className="col-10 text-center mt-5">
             <FacebookButton />
           </div>
         </div>
