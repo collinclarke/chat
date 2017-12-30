a32e1ea1fc44ba4aa43a2d4e8f8b25fb98b6d0bd
Collin <collinclarke@gmail.com>
2017-12-30 12:32:33 -0800

    Connect nav, replacing link copy with logo and image

--- src/Nav.js
+++ src/Nav.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import logoSmall from './assets/talk_small.svg';
 
 class Nav extends Component {
   render() {
+    const {photoURL} = this.props.currentUser;
     return (
-      <nav className="navbar fixed-top">
-        <Link className="navbar-brand" to="/chat" >chat</Link>
-        <Link className="navbar-brand m-0" to="/profile" >profile</Link>
+      <nav className="navbar fixed-top pt-0">
+        <Link className="navbar-brand" to="/chat" >
+          <img style={{width: "3rem"}} src={logoSmall}/>
+        </Link>
+        <Link className="m-0" to="/profile" >
+          <img src={photoURL}
+          style={{width: "3rem"}}
+          className="rounded-circle my-3"
+          alt="Profile"/>
+        </Link>
       </nav>
     )
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    currentUser: state.session.currentUser
+  }
+}
 
-export default Nav;
+export default connect(mapStateToProps, null)(Nav);
