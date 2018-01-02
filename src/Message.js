73ed24b5aad724467fc55e74c2ae0053d3cfc480
Collin <collinclarke@gmail.com>
2018-01-02 11:29:53 -0800

    Format messages to align and linebreak properly

--- src/Message.js
+++ src/Message.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Message extends Component {
 
   componentDidMount() {
-    this.refs.message.scrollIntoView({block: 'start', behavior: 'smooth'})
+    const { container, message, text } = this.refs;
+    message.scrollIntoView({block: 'start', behavior: 'smooth'})
+    container.style.width = ( text.offsetWidth + 2 )+ 'px';
   }
 
   render() {
@@ -16,7 +18,11 @@ class Message extends Component {
       <div className={"m-0 row " + direction}>
         <div className="col-auto mw-100 px-0">
           <div ref="message" className={"mx-2 my-1 px-2 d-flex justify-content-center " + bgColor} style={{borderRadius: '1rem', minWidth: '2rem'}}>
-            <div style={{wordBreak: 'break-all'}} className="text-left text-light m-1"> { this.props.text } </div>
+            <div ref="container"  className="text-left text-light m-1 mw-100">
+              <span ref="text" style={{wordWrap: 'break-word', whiteSpace: 'pre-wrap'}} className="mw-100">
+                { this.props.text }
+              </span>
+            </div>
           </div>
         </div>
       </div>
